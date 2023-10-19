e0b8fb18bd48cc0fec8a35e331be9a6bbcc3f098
Avraham Moshe <avimo26@gmail.com>
2023-10-19 01:42:37 -0500

    add other file

---
+++ confundo/common.py
@@ -0,0 +1,12 @@
+# -*- Mode: python; py-indent-offset: 4; indent-tabs-mode: nil; coding: utf-8; -*-
+# Copyright 2019 Alex Afanasyev
+#
+
+MTU=412
+MAX_SEQNO = 50000
+RETX_TIME = 0.5
+FIN_WAIT_TIME = 2.0
+SYN_WAIT_TIME = 10.0
+INIT_SSTHRESH = 12000
+GLOBAL_TIMEOUT = 10.0
+INIT_SEQ_NUM = 50000
---
+++ confundo/util.py
@@ -0,0 +1,14 @@
+
+MOD = 50000 + 1
+def format_line(command, pkt, cwnd, ssthresh):
+    s = f"{command} {pkt.seqNum} {pkt.ackNum} {pkt.connId} {int(cwnd)} {ssthresh}"
+    if pkt.isAck: s = s + " ACK"
+    if pkt.isSyn: s = s + " SYN"
+    if pkt.isFin: s = s + " FIN"
+    if pkt.isDup: s = s + " DUP"
+    return s
+def incSeqNum(seqNumber, bytes):
+    seqNumber += bytes
+    if seqNumber >= MOD:
+        seqNumber %= MOD
+    return seqNumber
